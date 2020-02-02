aa83375aa999e9659a3c4d6c3122f604ba71b4be
Schlizohr <moritzilg@hotmail.com>
2020-02-02 17:53:55 +0100

    Stage #3: Make it your own

--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -9,16 +9,39 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.all();
+        main.createCards();
+        main.askDefinitions();
     }
 
-    private void all() {
-        String s = "wrong...";
-        if (cardBox.createAndStoreCard(scanner.nextLine().trim(),
-                scanner.nextLine().trim())
-                .validateAnswer(scanner.nextLine().trim())) {
-            s = "right!";
+    private void createCards() {
+        System.out.println("Input the number of cards:");
+        int numberOfCards = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 1; i <= numberOfCards; i++) {
+            createCard(i);
         }
-        System.out.println("Your answer is " + s);
+    }
+
+    private void createCard(int i) {
+        System.out.println("The card #" + i);
+        String term = scanner.nextLine().trim();
+        System.out.println("The definition of the card #" + i);
+        String def = scanner.nextLine().trim();
+        cardBox.createAndStoreCard(term, def);
+    }
+
+    private void askDefinitions() {
+        for (FlashCard card : cardBox.getCards()) {
+            askDefinition(card);
+        }
+    }
+
+    private void askDefinition(FlashCard card) {
+        System.out.println("Print the definition of \"" + card.getTerm() + "\":");
+        if (card.validateAnswer(scanner.nextLine().trim())) {
+            System.out.println("Correct answer");
+        } else {
+            System.out.println("Wrong answer. The correct one is \"" + card.getDefinition() + "\".");
+        }
     }
 }
